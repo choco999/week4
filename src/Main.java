d51dfc718989539ba4f5b2fadb56f76c0eeebfd6
Chisato <49408516+choco999@users.noreply.github.com>
2021-02-11 14:54:10 -0500

    added JavaFX

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import models.GroceryItem;
 import models.Purchases;
 
-public class Main {
+public class Main extends Application {
 
     public static void main(String[] args) {
         GroceryItem item1 = new GroceryItem("carrots",4.79,"vegetables");
@@ -11,6 +16,16 @@ public class Main {
         shoppingCart.addGroceryItem(item1);
         shoppingCart.addGroceryItem(item2);
 
-
+        launch(args);
     }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("views/groceryCartView.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Grocery Extravaganza");
+        stage.show();
+    }
+
 }
